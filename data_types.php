a75f937f5a3405c9019099638e0ca1f0a9e221ac
AishaBukar <49945216+AishaBukar@users.noreply.github.com>
2023-02-03 21:31:30 +0100

    data types

---
+++ data_types.php
@@ -0,0 +1,30 @@
+<?php
+
+// This is an example of an integer data type
+//It must be a number without decimals
+$numbers= 20;
+
+// This is an example of a string
+//It is a simple sentence
+$sentence= "This is an example of a string";
+
+//This is an example of a float
+//It is a decimal number
+$numbers_with_points= 20.8;
+
+//Remember you can't name your variables with built in words
+
+//This is an example of a null or empty value
+$emptyvalue1= null;
+
+#This is an example of a boolean value
+#It's either true or false
+
+$decision= false;
+$decision_two= true;
+
+#We also have conditional values but won't go there now
+
+
+
+?>
